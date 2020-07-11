27b5888cda775bd8ea2dafc65f1a8a4357499566
mahallu-management <68157546+mahallu-management@users.noreply.github.com>
2020-07-11 20:49:49 +0530

    Add files via upload

---
+++ adminhome.php
@@ -0,0 +1,35 @@
+<?php include("adminheader.php")?>
+
+<img src="sss.jpg" width="300" height="300"/>
+<br>
+<br>
+
+<table class="table">
+		<tbody><tr>
+			<td>
+			<p align="justify"><b>MAHALLU MANAGEMENT  </b></p></td>
+		</tr>
+		<tr>
+			<td>
+			
+			<p align="justify"></p>
+			<ul>
+            
+
+
+
+
+				<li>
+				<p align="justify"> </p></li>
+				<li>
+				<p align="justify"></p></li>
+				<li>
+				<p align="justify"></p></li>
+				<li>
+				<p align="justify">
+</p></li>
+				<li>
+				<p align="justify"></p></li>
+			</ul>
+</td></tr></tbody></table>
+<?php include("footer.php")?>
